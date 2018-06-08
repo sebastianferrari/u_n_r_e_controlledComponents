0b7f8370aa6499c2222d46252f6096496930f110
Sebastian Ferrari <sebastianferrari@live.com>
2018-06-07 22:54:06 -0500

    refactored

--- src/App.js
+++ src/App.js
@@ -4,30 +4,6 @@ import './App.css';
 import ItemList from './ItemList';
 
 class App extends React.Component {
-
-  handleChange = event => {
-    this.setState({ value: event.target.value });
-  };
-
-  addItem = event => {
-    event.preventDefault();
-    this.setState(oldState => ({
-      items: [...oldState.items, this.state.value],
-    }));
-  };
-
-  deleteLastItem = event => {
-    this.setState(prevState => ({ items: this.state.items.slice(0, -1) }));
-  };
-
-  inputIsEmpty = () => {
-    return this.state.value === '';
-  };
-
-  noItemsFound = () => {
-    return this.state.items.length === 0;
-  };
-
   render() {
     return (
       <div className="App">
@@ -35,27 +11,7 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
-        //<h2>Shopping List</h2>
-        //<form onSubmit={this.addItem}>
-          //<input
-            //type="text"
-            //placeholder="Enter New Item"
-            //value={this.state.value}
-            //onChange={this.handleChange}
-          ///>
-          //<button disabled={this.inputIsEmpty()}>Add</button>
-        //</form>
-
-        //<button onClick={this.deleteLastItem} disabled={this.noItemsFound()}>
-          //Delete Last Item
-        //</button>
-
-        //<p className="items">Items</p>
-        //<ol className="item-list">
-          //{this.state.items.map((item, index) => <li key={index}>{item}</li>)}
-        //</ol>
-		
-		<ItemList />
+        <ItemList />
       </div>
     );
   }
--- src/ItemList.js
+++ src/ItemList.js
@@ -5,11 +5,34 @@ class ItemList extends Component {
     value: '',
     items: [],
   };
-  
+
+  handleChange = event => {
+    this.setState({ value: event.target.value });
+  };
+
+  addItem = event => {
+    event.preventDefault();
+    this.setState(oldState => ({
+      items: [...oldState.items, this.state.value],
+    }));
+  };
+
+  deleteLastItem = event => {
+    this.setState(prevState => ({ items: this.state.items.slice(0, -1) }));
+  };
+
+  inputIsEmpty = () => {
+    return this.state.value === '';
+  };
+
+  noItemsFound = () => {
+    return this.state.items.length === 0;
+  };
+
   render() {
-  return (
-    <div>
-    <h2>Shopping List</h2>
+    return (
+      <div>
+        <h2>Shopping List</h2>
         <form onSubmit={this.addItem}>
           <input
             type="text"
@@ -23,13 +46,13 @@ class ItemList extends Component {
         <button onClick={this.deleteLastItem} disabled={this.noItemsFound()}>
           Delete Last Item
         </button>
-    <p className="items">Items</p>
-    <ol className="item-list">
-      {this.state.items.map((item, index) => <li key={index}>{item}</li>)}
-    </ol>
-</div>
-  )
-}
+        <p className="items">Items</p>
+        <ol className="item-list">
+          {this.state.items.map((item, index) => <li key={index}>{item}</li>)}
+        </ol>
+      </div>
+    )
+  }
 }
 
 export default ItemList;
