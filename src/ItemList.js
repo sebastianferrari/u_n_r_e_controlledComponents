99016aaf1f4ceddc9e2d9c3316f2e30e3ad0c698
Sebastian Ferrari <sebastianferrari@live.com>
2018-06-08 03:40:30 +0000

    initial commit with some commented parts

---
+++ src/ItemList.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class ItemList extends Component {
+  state = {
+    value: '',
+    items: [],
+  };
+  
+  render() {
+  return (
+    <div>
+    <h2>Shopping List</h2>
+        <form onSubmit={this.addItem}>
+          <input
+            type="text"
+            placeholder="Enter New Item"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          <button disabled={this.inputIsEmpty()}>Add</button>
+        </form>
+
+        <button onClick={this.deleteLastItem} disabled={this.noItemsFound()}>
+          Delete Last Item
+        </button>
+    <p className="items">Items</p>
+    <ol className="item-list">
+      {this.state.items.map((item, index) => <li key={index}>{item}</li>)}
+    </ol>
+</div>
+  )
+}
+}
+
+export default ItemList;
